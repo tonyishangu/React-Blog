9878f71ccbcc70f98a7d6c6101ccb691487798cd
Tonnie Ishangu <67873141+tonyishangu@users.noreply.github.com>
2021-03-24 13:15:11 +0300

    add react blog post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,235 @@
-import logo from './logo.svg';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
 import './App.css';
+// import classes from '*.module.css';
+import { Typography, Container, Grid, Card, CardActionArea, CardContent } from '@material-ui/core';
+import CardMedia from '@material-ui/core/CardMedia'
+import Box from '@material-ui/core/Box'
+import Avatar from '@material-ui/core/Avatar'
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
+import CardActions from '@material-ui/core/CardActions'
+import Pagination from '@material-ui/lab/Pagination'
+
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: "#fff"
+  },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1558981852-426c6c22a060?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')`,
+    height: "500px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+    fontSize: "4rem",
+    [theme.breakpoints.down("sm")]: {
+      height: 300,
+      fontSize: "3em"
+    }
+  },
+  blogsContainer: {
+    paddingTop: theme.spacing(3)
+  },
+  blogTitle: {
+    fontWeight: 800,
+    paddingBottom: theme.spacing(3),
+    fontFamily:"cursive"
+  },
+  card: {
+    maxWidth: "100%",
+  },
+  media: {
+    height: 240
+  },
+  cardActions: {
+    display: "flex",
+    margin: "0 10px",
+    justifyContent: "space-between"
+  },
+  author: {
+    display: "flex"
+  },
+  paginationContainer: {
+    display: "flex",
+    justifyContent: "center"
+  }
+}));
 
 function App() {
+  const classes = useStyles()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar className={classes.appBar} position="static"> 
+        <Toolbar>
+          <Typography variant="h6" color="primary">
+            Blog
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box className={classes.hero}>
+        <Box>My Blog</Box>
+      </Box>
+      <Container maxWidth="lg" className={classes.blogsContainer}>
+        <Typography className={classes.blogTitle}>
+          Articles
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                    React useContext
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                  </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Box className={classes.author}>
+                  <Avatar src="https://res.cloudinary.com/dvxr1zote/image/upload/v1604400472/wc0iepha6wgcatkabkkl.jpg" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      Tonnie Ishangu
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                      Mar, 2021
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <BookmarkBorderIcon />
+                </Box>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="https://images.pexels.com/photos/34600/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    React Router
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions className={classes.cardActions}>
+                <Box className={classes.author}>
+                  <Avatar src="https://res.cloudinary.com/dvxr1zote/image/upload/v1604400472/wc0iepha6wgcatkabkkl.jpg" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      Tonnie Ishangu
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                      Mar 24, 2021
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <BookmarkBorderIcon />
+                </Box>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="https://images.pexels.com/photos/1181263/pexels-photo-1181263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    React useContext
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions className={classes.cardActions}>
+                <Box className={classes.author}>
+                  <Avatar src="https://res.cloudinary.com/dvxr1zote/image/upload/v1604400472/wc0iepha6wgcatkabkkl.jpg" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      Tonnie Ishangu
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                     Mar 24, 2021
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <BookmarkBorderIcon />
+                </Box>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="https://images.pexels.com/photos/325111/pexels-photo-325111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    React useContext
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions className={classes.cardActions}>
+                <Box className={classes.author}>
+                  <Avatar src="https://res.cloudinary.com/dvxr1zote/image/upload/v1604400472/wc0iepha6wgcatkabkkl.jpg" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      Tonnie Ishangu
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                      Mar 24, 2021
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <BookmarkBorderIcon />
+                </Box>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+      <Box my={4} className={classes.paginationContainer}>
+          <Pagination count={5} />
+        </Box>
     </div>
   );
 }
---
+++ src/Theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const Theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#764abc"
+        }
+    },
+    background: {
+        default: "#fff"
+    }
+})
+
+export default Theme;
